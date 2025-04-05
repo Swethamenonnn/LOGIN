251da6e571f8e9acdd8d6c2ae6d40d12e8f476ad
Swetha Menon <swethamenon2003@gmail.com>
2025-04-05 16:22:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function clickSubmitButton(event)
     event.preventDefault();
     const username= document.getElementById("username").value;
     const welcomeMessage= document.getElementById('welcome-message');
-    welcomeMessage.textcontent= 'welcome,' +username + '!';
+    welcomeMessage.textContent= 'welcome,' +username + '!';
     welcomeMessage.style.display= 'block';
 }
